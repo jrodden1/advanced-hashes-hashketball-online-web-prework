a4988b5ceb50de83d2801efbcec615e84d3f2c5e
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-03 18:05:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -202,3 +202,17 @@ def player_numbers(team)
   end
   jerseyNumbersArr
 end
+
+def player_stats(player_name)
+  outputHash = {}
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      data.each do |player, stats|
+        if player == player_name
+          outputHash = stats
+        end
+      end
+    end
+  end
+  outputHash
+end
