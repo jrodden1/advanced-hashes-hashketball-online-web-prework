b4157a5a370721a379bc80fee49b872545378888
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-03 16:25:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,34 +1,34 @@
 def game_hash
   hash = {
-    home: {
-      team_name: "Brooklyn Nets"
-      colors: ["Black","White"]
-      players: {
+    ::home => {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black","White"]
+      :players => {
         "Alan Anderson" => {
-          number: 0
-          shoe: 16
-          points: 22
-          rebounds: 12
-          assists: 12
-          steals: 3
-          blocks: 1
-          slam_dunks: 1
+          :number => 0
+          :shoe => 16
+          :points => 22
+          :rebounds => 12
+          :assists => 12
+          :steals => 3
+          :blocks => 1
+          :slam_dunks => 1
         }
       }
     }
-    away: {
-      team_name: "Charlotte Hornets"
-      colors: ["Turquoise","Purple"]
-      players: {
+    away => {
+      :team_name => "Charlotte Hornets"
+      :colors => ["Turquoise","Purple"]
+      :players => {
         "Jeff Adrien" => {
-          number: 4
-          shoe: 18
-          points: 10
-          rebounds: 1
-          assists: 1
-          steals: 2
-          blocks: 7
-          slam_dunks: 2
+          :number => 4
+          :shoe => 18
+          :points => 10
+          :rebounds => 1
+          :assists => 1
+          :steals => 2
+          :blocks => 7
+          :slam_dunks => 2
         }
       }
     }
