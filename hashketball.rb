e7f3661a6013b5171b6652e00d3b89f23d634818
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-03 16:19:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,36 @@
-# Write your code here!
-
-
-
-
-
-
-
-
-
-
+def game_hash
+  hash = {
+    home: {
+      team_name: ""
+      colors: ["",""]
+      players: {
+        "Player 1" => {
+          number: 0
+          shoe: 1
+          points: 1
+          rebounds: 1
+          assists: 1
+          steals: 1
+          blocks: 1
+          slam_dunks: 1
+        }
+      }
+    }
+    away: {
+      team_name: ""
+      colors: ["",""]
+      players: {
+        "Player 1" => {
+          number: 0
+          shoe: 1
+          points: 1
+          rebounds: 1
+          assists: 1
+          steals: 1
+          blocks: 1
+          slam_dunks: 1
+        }
+      }
+    }
+  }
+end
